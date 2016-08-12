88a03af1c8d1df1a532f85dc689df03e856a1dd4
PeterJ504 <peterjalderton@gmail.com>
2016-08-11 18:33:49 -0600

    Initial commit

--- MainActivity.java
+++ MainActivity.java
@@ -1,7 +1,11 @@
 package pja_dma.samplenotifications;
-
-import android.support.v7.app.AppCompatActivity;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createNotification();
+            }
+        });
+
+
+    }
+
+    public void createNotification() {
+
+        NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(this);
+        nBuilder.setContentTitle("Content title");
+        nBuilder.setContentText("This is the text");
+        nBuilder.setSmallIcon(R.drawable.ic_stat_name);
+
+        Notification notification = nBuilder.build();
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        nm.notify(1, notification);
+        // show notification
+
     }
 }
